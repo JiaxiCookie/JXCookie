8c6e4453fd2de7c56afcc17945fa74fd14f33045
Jessie Chen <jessiechen@Jessies-MacBook-Pro.local>
2024-12-14 17:54:16 +0800

    modify somewhere

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,7 +3,7 @@ import{LucideMailPlus} from "lucide-react"
 const Header = () => {
     return(
         <div className="sticky top-5 p-5 flex justify-between items-start max-w-7xl mx-auto z-20">
-            <div>jessies page</div>
+            <div>jessies blog page</div>
             <div><LucideMailPlus /></div>
         </div>
 
